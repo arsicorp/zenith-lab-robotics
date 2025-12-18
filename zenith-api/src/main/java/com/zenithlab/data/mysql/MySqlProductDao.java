4d536ec658fe59cd7c5efa3c73a8bc7374c602ae
Arisu <oshosh12909@gmail.com>
2025-12-18 02:38:00 -0800

    bug 2 was fixed, product update now updates instead of creating duplicates

--- zenith-api/src/main/java/com/zenithlab/data/mysql/MySqlProductDao.java
+++ zenith-api/src/main/java/com/zenithlab/data/mysql/MySqlProductDao.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Component
 public class MySqlProductDao extends MySqlDaoBase implements ProductDao
 {
-    public MySqlProductDao(DataSourceq dataSource)
+    public MySqlProductDao(DataSource dataSource)
     {
         super(dataSource);
     }
